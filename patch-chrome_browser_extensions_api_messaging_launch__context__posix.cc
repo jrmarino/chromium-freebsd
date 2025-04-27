e7f3714a88462e87518fdf2d742c2ab84c3abb12
John Marino <draco@marino.st>
2025-04-27 18:49:28 -0500

    import v128.0.6613.137

--- patch-chrome_browser_extensions_api_messaging_launch__context__posix.cc
+++ patch-chrome_browser_extensions_api_messaging_launch__context__posix.cc
@@ -1,6 +1,6 @@
---- chrome/browser/extensions/api/messaging/native_process_launcher_posix.cc.orig	2024-06-17 12:56:06 UTC
-+++ chrome/browser/extensions/api/messaging/native_process_launcher_posix.cc
-@@ -84,7 +84,7 @@ bool NativeProcessLauncher::LaunchNativeProcess(
+--- chrome/browser/extensions/api/messaging/launch_context_posix.cc.orig	2024-08-26 12:06:38 UTC
++++ chrome/browser/extensions/api/messaging/launch_context_posix.cc
+@@ -86,7 +86,7 @@ std::optional<LaunchContext::ProcessState> LaunchConte
  
  // TODO(crbug.com/40118868): Revisit the macro expression once build flag switch
  // of lacros-chrome is complete.
